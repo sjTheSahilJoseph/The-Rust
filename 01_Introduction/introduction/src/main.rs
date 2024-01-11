51f747e8257f7a9e35430fe45cf9f03ad3d34e9a
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-01-11 17:25:40 +0500

    Introduction

--- 01_Introduction/introduction/src/main.rs
+++ 01_Introduction/introduction/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Rust.
+    // Rust is a Programming Language.
+
+    // Why Rust?
+    // Rust is the fastest language after C.
+    // Rich type system.
+    // No garbage collector. (Faster Runtime).
+    // Useful Compiler Output.
+    // Memory Safety.
+
 }
